378e220d3c95ad0e3a0be15f174a11c534e80883
Bakora2001 <maxwellbakora@gmail.com>
2023-07-21 16:32:12 +0300

    Javascript part

--- learn.js
+++ learn.js
@@ -1,4 +1,8 @@
+const NewYears = '1st Jan 2024';
 
+function countdown(){
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
 
-
-
+    console.log(newYearsDate - currentDate);
+}
